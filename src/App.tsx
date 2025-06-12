d75b8ef4b5c59dcc9a4b44ea836386c3c2fd89a0
unknown <lenalaptev13@gmail.com>
2025-06-12 16:30:03 +0900

    changed detail

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,24 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import Button from "@mui/material/Button"
 import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 import Item from "./Item";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 
 const client = generateClient<Schema>();
+
+const todoSchema = z.object({
+  task: z.string().min(1, "タスクを入力してください")
+})
+
+type TodoSchema = z.infer<typeof todoSchema>;
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const { user, signOut } = useAuthenticator();
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -24,11 +31,17 @@ function App() {
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
+    console.log(client.models);
+    
+  }
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
   }
 
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <Typography variant="h4" component="h2">{user?.signInDetails?.loginId}'s todos</Typography>
       <button onClick={createTodo}>+ new</button>
       <Stack spacing={1}>
         {todos.map((todo) => (
